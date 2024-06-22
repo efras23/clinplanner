900361d7262bef1ff537c7bd0c7d8de1c41b248c
Efraim <efra10_efraim@hotmail.com>
2024-06-22 06:09:45 -0300

    upload das (principais) páginas HTML e recursos

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express")
 const app = express()
+const mustacheExpress = require("mustache-express")
 
-const PORT = 2024
+//renderizar páginas HTML com mustache-express
+app.engine("html", mustacheExpress())
+app.set("view engine", "html")
+app.set("views", __dirname + "/src/views")
 
-app.get("/", function(req, resp){
-    resp.send("Teste.")
+//servir arquivos estáticos (como CSS)
+app.use(express.static("./src/views"))
+//pegar dados de formulário
+app.use(express.urlencoded({extended: true}))
+//direcionar rotas para módulo adequado
+//app.use("/", require("./src/routes/RoutesClin"))
+
+app.get("/", function(req, res){
+    res.render("index.html")
 })
 
+const PORT = 2024
 app.listen(PORT, function(req, res){
     console.log("Aplicação rodando na porta " + PORT)
 })
