e70a5b2a5a8c1996166e30a50eee7a1746458970
ArthurAlvesz <arthuralvesds4@gmail.com>
2024-06-22 19:30:33 -0300

    Realizando alteracoes no projeto

--- src/controllers/autenticacaoController.js
+++ src/controllers/autenticacaoController.js
@@ -1,19 +1,39 @@
 const Usuario = require('../model/usuario');
 
-function autenticar(req, res){
-    const usuario = { 
+async function autenticar(req, res){
+    const usuario = await Usuario.findOne({ where: {
         email: req.body.email, 
-        senha: req.body.senha
-}};
-
-if(usuario !== nulo){
-    req.autorizado = true;
-    req.usuario = usuario;
-    res.redirect('/especialidades');
+        senha: req.body.senha}
+    });
+    if(usuario !== null){
+        req.session.autorizado = true;
+        req.session.usuario = usuario;
+        res.redirect('/especialidades');
+    }
+    else{
+        let erro_autenticacao = true;
+        res.render('index.html', {erro_autenticacao});
+    }
 }
-else{
-    let erro_autenticacao = true;
-    res.render('index.html', {erro_autenticacao});
+
+function verificarAutenticacao(req, res, next) {
+    if(req.session.autorizado){
+        console.log("usuário autorizado");
+        next();
+    }
+    else{
+        console.log("usuário NÃO autorizado");
+        res.redirect('/');
+    }   
 }
 
+function sair(req, res) {
+    req.session.destroy();
+    res.redirect('/');
+}
 
+module.exports = {
+    autenticar,
+    verificarAutenticacao,
+    sair
+}
